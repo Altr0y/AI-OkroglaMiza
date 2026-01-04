817553a03144183b1aee67e89c8657bd041af0c9
marusamataj <marusa.mataj@student.um.si>
2026-01-04 15:14:52 +0100

    feat: Add Forgot Password Page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,20 +1,6 @@
 'use client';
 
-import { useState } from 'next';
-import { useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
-
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 
 import { LoginForm } from '@/components/login-form';
 
--- frontend/components/login-form.tsx
+++ frontend/components/login-form.tsx
@@ -47,7 +47,7 @@ export function LoginForm({
                 <div className='flex items-center'>
                   <FieldLabel htmlFor='password'>{t('password')}</FieldLabel>
                   <a
-                    href='#'
+                    href='/forgot-password'
                     className='ml-auto inline-block text-sm underline-offset-4 hover:underline'
                   >
                     {t('forgotPassword')}
