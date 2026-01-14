75cc911811d545a8b10abb04abf66a9e25d89532
marusamataj <marusa.mataj@student.um.si>
2026-01-14 09:33:50 +0100

    feat: Split into Landing and Prompt component

---
+++ frontend/components/prompt.tsx
@@ -0,0 +1,11 @@
+import { useTranslations } from 'next-intl';
+
+export function Prompt() {
+  const t = useTranslations('HomePage');
+  return (
+
+          <div className="flex w-full gap-8">
+            prompt
+          </div>
+  );
+}
