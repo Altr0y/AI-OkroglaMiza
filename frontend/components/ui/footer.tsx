4aa7e0bc363f1bb6bca1d52e614befea123e3784
teodorus12 <34840935+teodorus12@users.noreply.github.com>
2026-01-14 19:40:36 +0100

    Finalized about us and fixed localization, then added them to dev (#22)

--- frontend/components/ui/footer.tsx
+++ frontend/components/ui/footer.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 
 import { Button } from '@/components/ui/button';
@@ -52,21 +51,6 @@ export function Footer() {
           </div>
         </div>
       )}
-
-
-=======
-export function Footer() {
-  return (
-    <footer className='w-full border-t bg-white dark:bg-black'>
-      <div className='flex h-16 items-center justify-center px-4 w-full'>
-        <div className='flex items-center'>
-          <span className='text-sm text-zinc-600 dark:text-zinc-400'>
-            © {new Date().getFullYear()} Razvoj programske opreme - Group 5. All
-            rights reserved.
-          </span>
-        </div>
-      </div>
->>>>>>> dev
     </footer>
   );
 }
